8992a7a75e07d72ff77199c6381c9f4fe9de4054
Valentin <valentin.rudloff.perso@gmail.com>
2023-06-13 12:16:28 +0200

    update

--- talksmith/core/run.py
+++ talksmith/core/run.py
@@ -1,36 +1,48 @@
 from langchain import OpenAI
 from langchain.memory import ConversationBufferMemory
+import os
+import asyncio
+import datetime
 
 import talksmith.utils.configuration as configuration
 from talksmith.core.bot import TalksmithBot
-
-# Run async function synchronously
-def run_async(coroutine_object):
-    return asyncio.get_event_loop().run_until_complete(coroutine_object)
+from talksmith.hmi.chat import CLI
+from talksmith.core.io import export_chat_history
 
 class Core:
 
     def __init__(self):
         """Get the configuration and init the bot, its memory and its chains"""
-        conf = configuration.load()
+        self.conf = configuration.load()
         print(conf)
-        exit(0)
-        chatgpt_chain = TalksmithBot(OpenAI(temperature=0), ConversationBufferMemory(return_messages=True, human_prefix="human", ai_prefix="ai", memory_key="memory"))
+        self.hmi = CLI()
+        self.bot = TalksmithBot(OpenAI(temperature=0),
+                                          ConversationBufferMemory(
+                                              return_messages=True,
+                                              human_prefix="human",
+                                              ai_prefix="ai",
+                                              memory_key="memory"))
+
+    def check_for_exit(self, user_input: str) -> bool:
+        if user_input.lower() == 'exit':
+                export_input = input("Nice talking to you. Do you want to export our conversation? (Y/N): ")
+                if export_input.lower() == 'y':
+                    # Access the memory
+                    memory = self.chatgpt_chain.memory.load_memory_variables({})
+                    # Save chat history in memory
+                    export_chat_history(memory, os.path.join(
+                        self.conf.chat_history_path,
+                        self.conf.chatbot_name,
+                        datetime.datetime.now()))
+                return True
+        return False
 
     def run(self):
         """Get user input and process it"""
         end_chat = False
         while not end_chat:
-            user_input = input(f"\nYour message: ")
-            if user_input.lower() == 'exit':
-                export_input = input(f"\nNice talking to you. Do you want to export our conversation? (Y/N): ")
-                if export_input.lower() == 'y':
-                    # Access the memory
-                    memory = chatgpt_chain.memory.load_memory_variables({})
-                    # Save chat history in memory
-                    export_chat_history(memory, "human_assistant_messages.txt")
+            user_input = self.hmi.get_user_input()
+            end_chat = self.check_for_exit(user_input)
+            error_message = self.bot.process_input(chatgpt_chain, user_input)
+            if error_message is not None:
                 end_chat = True
-            else:
-                error_message = run_async(process_input(chatgpt_chain, user_input))
-                if error_message is not None:
-                    end_chat = True
